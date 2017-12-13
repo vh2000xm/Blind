4647b41518f31ae8db3f2c0b789793884ae0a9dc
vh2000xm <vh2000xm@gmail.com>
2017-12-13 21:19:04 +0900

    Loading Time Change.

--- app/src/main/java/com/example/innoz/iotapplication/SplashActivity.java
+++ app/src/main/java/com/example/innoz/iotapplication/SplashActivity.java
@@ -3,9 +3,6 @@ package com.example.innoz.iotapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Ahn on 2017-12-13.
@@ -18,7 +15,7 @@ public class SplashActivity extends Activity {
         // MainActivity.class 자리에 다음에 넘어갈 액티비티를 넣어주기
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
