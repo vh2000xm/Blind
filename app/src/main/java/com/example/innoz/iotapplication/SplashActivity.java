a05daca71816569118b8dfe4d6e97efebc081f6a
vh2000xm <vh2000xm@gmail.com>
2017-12-19 01:22:43 +0900

    something chaned.

--- app/src/main/java/com/example/innoz/iotapplication/SplashActivity.java
+++ app/src/main/java/com/example/innoz/iotapplication/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         // MainActivity.class 자리에 다음에 넘어갈 액티비티를 넣어주기
         try {
-            Thread.sleep(4000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
