458a3981049f2c6ab1f5de2e814eb6ec7283dc2d
vh2000xm <vh2000xm@gmail.com>
2018-01-17 13:40:29 +0900

    버튼 누르면 자동으로 다음장 넘어가게 수정할것.

--- app/src/main/java/com/example/innoz/iotapplication/Help_Bluetooth_Activity.java
+++ app/src/main/java/com/example/innoz/iotapplication/Help_Bluetooth_Activity.java
@@ -27,18 +27,30 @@ public class Help_Bluetooth_Activity extends Activity {
     private static final String TAG = "Help_Bluetooth_Activity";
 
 
-    /**Bluetooth**/
+    /**
+     * Bluetooth
+     **/
     private BluetoothService btService = null;
     private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT = 2;
     private static final int ADDROOM_FINISH = 1;
 
 
-    /**Data Base**/
+    /**
+     * Data Base
+     **/
     private SQLiteService dbHelper = null;
 
-    // Layout
+    /**
+     * Data Base
+     **/
     Button btn_bluetooth;
+    Button btn_name;
+    Button btn_Next;
+    Button btn_test_start;
+    Button btn_test_stop;
+    Button btn_test_finish;
+    Button btn_help_finish;
     public String key = null;
 
 
@@ -64,22 +76,42 @@ public class Help_Bluetooth_Activity extends Activity {
                 .check();
 
 
-
         /** Sliding Layout **/
         SlidingView sv = new SlidingView(this);
-        View v1 = View.inflate(this, R.layout.help_bluetooth_v1, null);
-        View v2 = View.inflate(this, R.layout.help_bluetooth_v2, null);
-        sv.addView(v1);
-        sv.addView(v2);
+
+        View v[] = new View[5];
+        v[0] = View.inflate(this, R.layout.help_bluetooth_v1, null);
+        v[1] = View.inflate(this, R.layout.help_bluetooth_v2, null);
+        v[2] = View.inflate(this, R.layout.help_bluetooth_v3, null);
+        v[3] = View.inflate(this, R.layout.help_bluetooth_v4, null);
+        v[4] = View.inflate(this, R.layout.help_bluetooth_v5, null);
+
+        for (int i = 0; i < 5; ++i)
+            sv.addView(v[i]);
+
         setContentView(sv);
 
 
         /** Main Layout **/
-        btn_bluetooth = (Button)findViewById(R.id.bluetooth_detect);
+        btn_bluetooth = (Button) findViewById(R.id.bluetooth_detect);
+        btn_name = (Button) findViewById(R.id.btn_nameok);
+        btn_Next = (Button) findViewById(R.id.btn_NextStep);
+        btn_test_start = (Button) findViewById(R.id.btn_testStart);
+        btn_test_stop = (Button) findViewById(R.id.btn_testStop);
+        btn_test_finish = (Button) findViewById(R.id.btn_testFinish);
+        btn_help_finish = (Button) findViewById(R.id.btn_helpFinish);
+
+        /** Listener **/
         btn_bluetooth.setOnClickListener(viewOnClickListener);
+        btn_name.setOnClickListener(viewOnClickListener);
+        btn_Next.setOnClickListener(viewOnClickListener);
+        btn_test_start.setOnClickListener(viewOnClickListener);
+        btn_test_stop.setOnClickListener(viewOnClickListener);
+        btn_test_finish.setOnClickListener(viewOnClickListener);
+        btn_help_finish.setOnClickListener(viewOnClickListener);
 
         /** BT Service **/
-        if(btService == null) {
+        if (btService == null) {
             btService = new BluetoothService(this, mHandler);
         }
 
@@ -90,7 +122,9 @@ public class Help_Bluetooth_Activity extends Activity {
 
     }
 
-    /** BT Permission Check **/
+    /**
+     * BT Permission Check
+     **/
     PermissionListener permissionlistener = new PermissionListener() {
         @Override
         public void onPermissionGranted() {
@@ -104,13 +138,32 @@ public class Help_Bluetooth_Activity extends Activity {
         }
     };
 
-    /** btn Click listener **/
-    View.OnClickListener viewOnClickListener = new View.OnClickListener(){
+    /**
+     * btn Click listener
+     **/
+    View.OnClickListener viewOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             int id = v.getId();
-            switch (id){
+            switch (id) {
+/**버튼 누르면 다음장 자동으로 넘어가게  SlidingView 클래스 수정할것.**/
+                case R.id.btn_nameok:
+                    if (btService.getDeviceState()) {
+                        btService.enableBluetooth();
+                    } else {
+                        finish();
+                    }
+                    break;
+
                 case R.id.bluetooth_detect:
+                    if (btService.getDeviceState()) {
+                        btService.enableBluetooth();
+                    } else {
+                        finish();
+                    }
+                    break;
+
+                case R.id.btn_NextStep:
                     if (btService.getDeviceState()) {
                         btService.enableBluetooth();
                     } else {
@@ -125,7 +178,9 @@ public class Help_Bluetooth_Activity extends Activity {
     };
 
 
-    /** After BT Choice**/
+    /**
+     * After BT Choice
+     **/
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d(TAG, "onActivityResult " + resultCode);
 
