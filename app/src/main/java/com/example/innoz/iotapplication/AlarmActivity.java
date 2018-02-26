9bafa37a0eb281859a0288849c871f62304de38e
vh2000xm <vh2000xm@gmail.com>
2018-02-26 16:38:07 +0900

    180226
     To Do
    
     1.USER-Setting Option Activity : 기능 동작하게
    
     2.알람쪽은 - 기능동작하게

--- app/src/main/java/com/example/innoz/iotapplication/AlarmActivity.java
+++ app/src/main/java/com/example/innoz/iotapplication/AlarmActivity.java
@@ -1,8 +1,10 @@
 package com.example.innoz.iotapplication;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
@@ -25,15 +27,23 @@ public class AlarmActivity  extends AppCompatActivity {
     String TAG = "AlarmActivity";
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         WindowManager.LayoutParams layoutParams= new WindowManager.LayoutParams();
         getWindow().setAttributes(layoutParams);
         setContentView(R.layout.activity_alarm);
+
+
+
 
 //        btn_alram_25per = (ImageButton)findViewById(R.id.btn_arlam_25per);
 //        btn_alram_25per.setOnClickListener(viewOnClickListener);
