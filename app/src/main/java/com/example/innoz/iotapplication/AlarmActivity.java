f8b7af3f18a10f6fe60af46a7d3bd70a301bb61b
vh2000xm <vh2000xm@gmail.com>
2018-02-26 15:03:51 +0900

    180226
     To Do
    
     1.USER-Setting Option Activity : 기능 동작하게
    
     2.알람쪽은 - 기능동작하게
    
     방 여러가지로 움직일때 응답없음 Check.(태스크,타임아웃)

--- app/src/main/java/com/example/innoz/iotapplication/AlarmActivity.java
+++ app/src/main/java/com/example/innoz/iotapplication/AlarmActivity.java
@@ -1,6 +1,7 @@
 package com.example.innoz.iotapplication;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
@@ -17,7 +18,7 @@ import android.widget.NumberPicker;
  * Created by Ahn on 2018-01-06.
  */
 
-public class AlarmActivity  extends Activity {
+public class AlarmActivity  extends AppCompatActivity {
 
 
     ImageButton btn_alram_25per;
@@ -29,27 +30,27 @@ public class AlarmActivity  extends Activity {
         super.onCreate(savedInstanceState);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         WindowManager.LayoutParams layoutParams= new WindowManager.LayoutParams();
         getWindow().setAttributes(layoutParams);
         setContentView(R.layout.activity_alarm);
 
-        btn_alram_25per = (ImageButton)findViewById(R.id.btn_arlam_25per);
-        btn_alram_25per.setOnClickListener(viewOnClickListener);
+//        btn_alram_25per = (ImageButton)findViewById(R.id.btn_arlam_25per);
+//        btn_alram_25per.setOnClickListener(viewOnClickListener);
 
 
     }
+
 
     View.OnClickListener viewOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             int id = v.getId();
             switch (id) {
-                case R.id.btn_arlam_25per:
-                    Log.d(TAG,"25per Clicked!!!");
-                    ///// 인자값 넘기기(주소, 방이름) 다이얼 만들기
-                    break;
+//                case R.id.btn_arlam_25per:
+//                    Log.d(TAG,"25per Clicked!!!");
+//                    ///// 인자값 넘기기(주소, 방이름) 다이얼 만들기
+//                    break;
             }
         }
     };
