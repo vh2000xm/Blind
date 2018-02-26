4f769b88cb9af5433011ba04106b6057af387e8a
vh2000xm <vh2000xm@gmail.com>
2018-02-26 09:38:48 +0900

    180225
     To Do
    
     1.USER-Setting Option Activity : 기능 동작하게
    
     2.알람쪽은 - 다이얼 방식으로. 시간설정. ImageButton 체크
    
     방 여러가지로 움직일때 응답없음 Check.(태스크,타임아웃)

--- app/src/main/java/com/example/innoz/iotapplication/AlarmActivity.java
+++ app/src/main/java/com/example/innoz/iotapplication/AlarmActivity.java
@@ -1,13 +1,16 @@
 package com.example.innoz.iotapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.NumberPicker;
 
 /**
@@ -15,6 +18,10 @@ import android.widget.NumberPicker;
  */
 
 public class AlarmActivity  extends Activity {
+
+
+    ImageButton btn_alram_25per;
+    String TAG = "AlarmActivity";
 
 
     @Override
@@ -28,7 +35,23 @@ public class AlarmActivity  extends Activity {
         getWindow().setAttributes(layoutParams);
         setContentView(R.layout.activity_alarm);
 
+        btn_alram_25per = (ImageButton)findViewById(R.id.btn_arlam_25per);
+        btn_alram_25per.setOnClickListener(viewOnClickListener);
+
 
     }
+
+    View.OnClickListener viewOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            switch (id) {
+                case R.id.btn_arlam_25per:
+                    Log.d(TAG,"25per Clicked!!!");
+                    ///// 인자값 넘기기(주소, 방이름) 다이얼 만들기
+                    break;
+            }
+        }
+    };
 
 }
